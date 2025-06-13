7b513f95f60cea593d7c66c6cba643339c541d95
DavidLancheros05 <david.dlls05@gmail.com>
2025-06-13 02:25:51 -0500

    Agrega package.json y scripts para deploy

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ import Register from './pages/Register';
 import Productos from './pages/Productos';
 import Carrito from './pages/Carrito';
 import Inicio from './pages/Inicio';
-
 import AdminDashboard from './pages/AdminDashboard';
 import LoginAdmin from './pages/Login';
 import AdminProductos from './pages/AdminProductos';
 import ProductoDetalle from './pages/ProductoDetalle';
-
-import ListadoProductos from './components/ListadoProductos'; // ✅ Nuevo componente reutilizable
+import ListadoProductos from './components/ListadoProductos';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -25,22 +23,24 @@ function App() {
     <AuthProvider>
       <CarritoProvider>
         <BrowserRouter>
-          <Header />
-          <main style={{ flex: 1, padding: '1rem' }}>
-            <Routes>
-              <Route path="/" element={<Inicio />} />
-              <Route path="/productos" element={<Productos />} />
-              <Route path="/carrito" element={<Carrito />} />
-              <Route path="/login" element={<LoginAdmin />} />
-              <Route path="/admin" element={<AdminDashboard />} />
-              <Route path="/admin/productos" element={<AdminProductos />} />
-              <Route path="/bicicletas" element={<ListadoProductos tipo="bicicleta" />} />
-              <Route path="/accesorios" element={<ListadoProductos tipo="accesorios" />} />
-              <Route path="/producto/:id" element={<ProductoDetalle />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </main>
-          <Footer />
+          <div className="flex flex-col min-h-screen bg-gray-50">
+            <Header />
+            <main className="flex-grow px-4 py-6 container mx-auto">
+              <Routes>
+                <Route path="/" element={<Inicio />} />
+                <Route path="/productos" element={<Productos />} />
+                <Route path="/carrito" element={<Carrito />} />
+                <Route path="/login" element={<LoginAdmin />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="/admin/productos" element={<AdminProductos />} />
+                <Route path="/bicicletas" element={<ListadoProductos tipo="bicicleta" />} />
+                <Route path="/accesorios" element={<ListadoProductos tipo="accesorios" />} />
+                <Route path="/producto/:id" element={<ProductoDetalle />} />
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
         </BrowserRouter>
       </CarritoProvider>
     </AuthProvider>
