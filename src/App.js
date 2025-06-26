19c85e3cadb8b5ae33b1e60a43f40159b4da6ad8
Daviddlls <david.dlls05@gmail.com>
2025-06-26 01:53:48 -0500

    Corrijo rutas en App.jsx y estructura final 26-06-25

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { CarritoProvider } from './context/CarritoContext';
-import { AuthProvider } from './context/AuthContext';
+import { CarritoProvider } from '../../context/CarritoContext';
+import { AuthProvider } from '../../context/AuthContext';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
