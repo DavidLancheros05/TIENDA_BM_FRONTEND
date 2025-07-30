e04b681e6eb598ef91fe9f772d2ed2716fdd32ab
Daviddlls <david.dlls05@gmail.com>
2025-07-29 22:27:09 -0500

    Versión nueva de mi front

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
- // vite.config.js
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // 👈 proxy /api a tu backend
-    },
-  },
+  base: '/', // para rutas correctas en producción
 });
